643962932bb0926918aea289da555e681e8d3b91
Spencer Plant <spencer.plant@shopify.com>
2019-08-19 20:27:42 -0400

    Auto-runs, can click to toggle weekday counter

--- days_until_bf.js
+++ days_until_bf.js
@@ -1,7 +1,33 @@
-window.onload = function countDays() {
-    var bfcm = new Date('November 29, 2019 00:00:00');
-    var difference_in_s = (bfcm - Date.now()) / 1000;
-    var seconds_in_a_day = 86400;
-    var days = Math.ceil(difference_in_s / seconds_in_a_day);
-    document.getElementById("days_until").innerHTML = days
+var bfcm = new Date('November 29, 2019 00:00:00');
+var seconds_in_a_day = 86400;
+var days_to_display = null;
+var displaying_weekdays = false;
+
+function toggleCountMethod() {
+	if (displaying_weekdays) {
+		setDisplay(countWeekDays())
+		document.getElementById("days_until").innerHTML = "weekdays until Black Friday"
+	} else {
+		setDisplay(countDays())
+		document.getElementById("days_until").innerHTML = "days until Black Friday"
+	}
+	displaying_weekdays = !displaying_weekdays
+}
+
+function countDays() {
+    difference_in_s = (bfcm - Date.now()) / 1000;
+    return Math.ceil(difference_in_s / seconds_in_a_day);
+}
+
+function countWeekDays() {
+	return Math.ceil(countDays() * 5 / 7)
+}
+
+function setDisplay(text) {
+	document.getElementById("countdown").innerHTML = text
+}
+
+window.onload = function main() {
+	days_to_display = countDays();
+	setInterval(setDisplay(days_to_display), seconds_in_a_day/24);
 }
